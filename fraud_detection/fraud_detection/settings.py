69d70480b646104b16ed912565436a0265f139d0
Jitendhar <bhanujitendhar27@gmail.com>
2025-03-04 09:42:50 +0530

    Removed confidential information

--- fraud_detection/fraud_detection/settings.py
+++ fraud_detection/fraud_detection/settings.py
@@ -141,4 +141,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "bhanujiten@gmail.com"
-EMAIL_HOST_PASSWORD = "iwnt gyaa yrqh qvrq" 
+EMAIL_HOST_PASSWORD = "#" 
